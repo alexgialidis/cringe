8606099d7ec561f781f943f7f7cdd8adf1738e98
hackermaan <alwee1995@gmail.com>
2018-02-28 14:27:58 +0200

    minor changes to search

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,10 @@
 
 @section ('content')
 
+@if (Auth::guard('human')->user())
+<h1>hhhhhhhhhhhhhhhhhhhhhhhhh</h1>
+@endif
+
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
     <div class="top-right links">
@@ -28,16 +32,18 @@
     <div class="title m-b-md">
         Cringe
     </div>
+
+    <form method="GET" action="/events/search" class="search-form">
+        <!-- {{ csrf_field() }} -->
+        <div class="form-group has-feedback">
+            <label for="search" class="sr-only">Search</label>
+            <input type="text" class="form-control" name="search" id="search" placeholder="search">
+            <span class="glyphicon glyphicon-search form-control-feedback"></span>
+        </div>
+    </form> 
 </div>
 
-<form method="GET" action="/events/search" class="search-form">
-    <!-- {{ csrf_field() }} -->
-    <div class="form-group has-feedback">
-        <label for="search" class="sr-only">Search</label>
-        <input type="text" class="form-control" name="search" id="search" placeholder="search">
-        <span class="glyphicon glyphicon-search form-control-feedback"></span>
-    </div>
-</form>  
+
 
 </div>
 
