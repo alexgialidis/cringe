0ed98c40877e7657675d9d31c9188b6eae849774
geofila <geofila@outlook.com.gr>
2018-03-04 17:14:34 +0200

    added profiles

---
+++ resources/views/human/profile.blade.php
@@ -0,0 +1,159 @@
+@extends('layout')
+
+@section('content')
+<style>
+.slidecontainer {
+    width: 100%;
+}
+
+.slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+}
+
+.slider:hover {
+    opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    background: #5cb85c;
+    cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    background: #4CAF50;
+    cursor: pointer;
+}
+</style>
+<div class="container">
+    <div class="row">
+
+        <div class ="col-md-10 col-md-offset-2">
+            <div>
+                <div class="panel-heading">
+                    <h3>My Profile</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+
+
+                        <div class=" col-md-9 col-lg-9 ">
+                            <table class="table table-user-information" id="humanData">
+                                <tbody>
+                                    <tr>
+                                        <td>First Name:</td>
+                                        <td>{{ $human->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Last Name:</td>
+                                        <td>{{ $human->surname }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Address</td>
+                                        <td>{{ $human->city }}, {{ $human->address }} {{ $human->number }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <tr>
+                                            <td>Zip</td>
+                                            <td>{{ $human->zip }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Email</td>
+                                            <td>{{ $human->email }}</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Wallet Points</td>
+                                            <td>{{ $human->points }}</td>
+                                        </tr>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Button to Open the Modal -->
+<button type="button" class="btn btn-success center-block" data-toggle="modal" data-target="#myModal">
+    Add Points
+</button>
+
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class= "container-fluid">
+            <div class="modal-header">
+                <h4 class="modal-title">Add Points</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+
+            <div class="slidecontainer">
+              <input type="range" min="1" max="200" value="50" class="slider" id="myRange">
+              <p>Points: <span id="demo"></span></p>
+            </div>
+
+            </div>
+            <!-- Modal footer -->
+
+            <div class="modal-footer">
+                    <button type="button" class="btn btn-success center-block" data-dismiss="modal" onclick= "addPoints()"><span class="glyphicon glyphicon-piggy-bank"></span> Add Points</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<script>
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+
+function addPoints(){
+    //alert(slider.value);
+    table= document.getElementById("humanData");
+    $.get("{{ URL::to('human/addPoints') }}",{points: slider.value},function (data){
+        table.deleteRow(6);
+        var row = table.insertRow(6);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        cell1.innerHTML = "Wallet Points";
+        cell2.innerHTML = data;
+    });
+}
+</script>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,165 +12,176 @@
 
     <!-- Styles -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-            <style>
-            html, body {
-                background-color: #fff;
-                color: black;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 1000;
-                height: 100vh;
-                margin: 0;
-            }
+    <style>
+    html, body {
+        background-color: #fff;
+        color: black;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 1000;
+        height: 100vh;
+        margin: 0;
+        overflow-x: hidden;
+    }
 
-            .full-height {
-                height: 100vh;
-            }
+    .full-height {
+        height: 100vh;
+    }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
 
-            .position-ref {
-                position: relative;
-            }
+    .position-ref {
+        position: relative;
+    }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
 
-            .content {
-                text-align: center;
-            }
+    .content {
+        text-align: center;
+    }
 
-            .title {
-                font-size: 84px;
-            }
+    .title {
+        font-size: 84px;
+    }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+    .m-b-md {
+        margin-bottom: 30px;
+    }
 
-            .dropbtn {
-                background-color: #4CAF50;
-                color: white;
-                padding: 16px;
-                font-size: 16px;
-                border: none;
-                cursor: pointer;
-            }
+    .dropbtn {
+        background-color: #4CAF50;
+        color: white;
+        padding: 16px;
+        font-size: 16px;
+        border: none;
+        cursor: pointer;
+    }
 
-            .dropdown {
-                position: relative;
-                display: inline-block;
-            }
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
 
-            .dropdown-menu {
-                display: none;
-                position: absolute;
-                right: 0;
-                background-color: #f9f9f9;
-                min-width: 160px;
-                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                z-index: 1;
-            }
+    .dropdown-menu {
+        display: none;
+        position: absolute;
+        right: 0;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
 
-            .dropdown-menu a {
-                color: black;
-                padding: 12px 16px;
-                text-decoration: none;
-                display: block;
-            }
+    .dropdown-menu a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
 
-            .dropdown-menu a:hover {background-color: #f1f1f1}
+    .dropdown-menu a:hover {background-color: #f1f1f1}
 
-            .dropdown:hover .dropdown-menu {
-                display: block;
-            }
+    .dropdown:hover .dropdown-menu {
+        display: block;
+    }
 
-            .dropdown:hover .dropbtn {
-                background-color: #3e8e41;
-            }
+    .dropdown:hover .dropbtn {
+        background-color: #3e8e41;
+    }
 
 
-        </style>
+    </style>
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
     <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-            ]); ?>
-        </script>
-    </head>
-    <body>
+    window.Laravel = <?php echo json_encode([
+        'csrfToken' => csrf_token(),
+    ]); ?>
+    </script>
+</head>
+<body>
 
-        <nav class="navbar navbar-inverse">
-          <div class="container-fluid">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>                        
-            </button>
-            <a class="navbar-brand" href="#">Cringe</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class=""><a href="/">Home</a></li>
-            <li><a href="/events">Events</a></li>
-            <li><a href="#">Profile</a></li>
-        </ul>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Cringe</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class=""><a href="/">Home</a></li>
+                    <li><a href="/events">Events</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
 
-        <!-- Right Side Of Navbar -->
-        <ul class="nav navbar-nav navbar-right">
-            <!-- Authentication Links -->
-            @if (Auth::guard('provider')->user())
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    {{ Auth::guard('provider')->user()->name }} <span class="caret"></span>
-                </a>
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guard('provider')->user())
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::guard('provider')->user()->name }} <span class="caret"></span>
+                        </a>
 
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        <a href="{{ url('/provider/logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ url('/provider/profile') }}">
+                                My Profile
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/provider/logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
 
-                    <form id="logout-form" action="{{ url('/provider/logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            </ul>
-        </li>
-        @elseif (Auth::guard('human')->user())
-              <li class="dropdown">
+                        <form id="logout-form" action="{{ url('/provider/logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </li>
+            @elseif (Auth::guard('human')->user())
+            <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                     {{ Auth::guard('human')->user()->name }} <span class="caret"></span>
                 </a>
 
                 <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{ url('/human/profile') }}">
+                        My Profile
+                    </a>
+                    </li>
                     <li>
                         <a href="{{ url('/human/logout') }}"
                         onclick="event.preventDefault();
@@ -185,48 +196,48 @@
             </ul>
         </li>
         @elseif (Auth::guard('admin')->user())
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
+            </a>
+
+            <ul class="dropdown-menu" role="menu">
+                <li>
+                    <a href="{{ url('/admin/logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    Logout
                 </a>
 
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        <a href="{{ url('/admin/logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
+                <form id="logout-form" action="{{ url('/admin/logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </li>
+        </ul>
+    </li>
+    @else
 
-                    <form id="logout-form" action="{{ url('/admin/logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
+    <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#"> <span class="glyphicon glyphicon-log-in"></span> Sign Up
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ url('/provider/register') }}">Provider</a></li>
+                    <li><a href="{{ url('/human/register') }}">Parent</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#"> <span class="glyphicon glyphicon-log-in"></span> Log In
+                    <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{ url('/provider/login') }}">Provider</a></li>
+                        <li><a href="{{ url('/human/login') }}">Parent</a></li>
+                    </ul>
                 </li>
             </ul>
-        </li>
-            @else
-
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"> <span class="glyphicon glyphicon-log-in"></span> Sign Up
-                        <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ url('/provider/register') }}">Provider</a></li>
-                            <li><a href="{{ url('/human/register') }}">Parent</a></li>
-                      </ul>
-                  </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"> <span class="glyphicon glyphicon-log-in"></span> Log In
-                        <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ url('/provider/login') }}">Provider</a></li>
-                            <li><a href="{{ url('/human/login') }}">Parent</a></li>
-                      </ul>
-                  </li>
-              </ul>
-        @endif
-    </ul>
-</div>
+            @endif
+        </ul>
+    </div>
 </div>
 </nav>
 
@@ -237,8 +248,8 @@
 </body>
 <script>
 $(document).ready(function() {
-  $('li.active').removeClass('active');
-  $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 });
 </script>
 </html>
